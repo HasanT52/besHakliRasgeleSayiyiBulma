068aebca24811da1082e1822573d9b23cf372165
hasan <hasan@DESKTOP-2SJRI5S>
2022-11-30 18:53:09 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_random = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_random
+            // 
+            this.btn_random.Location = new System.Drawing.Point(304, 82);
+            this.btn_random.Name = "btn_random";
+            this.btn_random.Size = new System.Drawing.Size(75, 23);
+            this.btn_random.TabIndex = 1;
+            this.btn_random.Text = "Sayı Ver";
+            this.btn_random.UseVisualStyleBackColor = true;
+            this.btn_random.Click += new System.EventHandler(this.btn_random_Click);
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(205, 110);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 23);
+            this.btn_play.TabIndex = 2;
+            this.btn_play.Text = "Oyna";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.btn_random);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_random;
+        private System.Windows.Forms.Button btn_play;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int tahminsayisi;
+        int i = 5;
+        
+        private void btn_random_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            tahminsayisi = random.Next(1, 100);
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            tahminDogruMu(sayi);
+        }
+
+        void tahminDogruMu (int sayi)
+        {
+            if(i > 0)
+            {
+                if (sayi < tahminsayisi)
+                {
+                    MessageBox.Show("Tahminizi Yükseltin, kalan hakkınız: " + i);
+                    i = i - 1;
+                }
+
+                else if (sayi > tahminsayisi)
+                {
+                    MessageBox.Show("Tahminizi düşürün, kalan hakkınız: " + i);
+                    i = i - 1;
+                }
+
+                else
+                {
+                    MessageBox.Show("Tebrikler doğru tahmin");                   
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("GameOver");                
+            }
+            
+        }
+
+    }
+}
